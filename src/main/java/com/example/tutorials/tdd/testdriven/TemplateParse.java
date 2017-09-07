de2a5f12bd8392a0dc35c3412bf4fab141112e12
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 15:44:29 +0800

    New failing test TestTemplateParse

---
+++ src/main/java/com/example/tutorials/tdd/testdriven/TemplateParse.java
@@ -0,0 +1,13 @@
+package com.example.tutorials.tdd.testdriven;
+
+import java.util.List;
+
+public class TemplateParse {
+	public TemplateParse() {
+		
+	}
+	
+	List<String> parse(String something) {
+		return null;
+	}
+}
