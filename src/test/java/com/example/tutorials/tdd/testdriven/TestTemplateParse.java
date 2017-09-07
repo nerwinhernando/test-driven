cb9b964ee9fd9dc291aecd2ab7c86088a7456134
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 16:42:16 +0800

    Refactor to assertSegments

--- src/test/java/com/example/tutorials/tdd/testdriven/TestTemplateParse.java
+++ src/test/java/com/example/tutorials/tdd/testdriven/TestTemplateParse.java
@@ -2,26 +2,32 @@ package com.example.tutorials.tdd.testdriven;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
 public class TestTemplateParse {
+		
 	@Test
 	public void emptyTemplateRendersAsEmptyString() throws Exception {
 		List<String> segments = parse("");
-		assertEquals("Number of segments", 1, segments.size());
-		assertEquals("", segments.get(0));
+		assertSegments(segments, "");
 	}
 	
 	@Test
 	public void templateWithOnlyPlainText() throws Exception {
 		List<String> segments = parse("plain text only");
-		assertEquals("Number of segments", 1, segments.size());
-		assertEquals("plain text only", segments.get(0));
+		assertSegments(segments, "plain text only");
 	}
 
 	private List<String> parse(String template) {
 		return new TemplateParse().parse(template);
 	}
+	
+	private void assertSegments(List<? extends Object> actual, Object ... expected) {
+		assertEquals("Number of segments doesn't match.", expected.length, actual.size());
+		assertEquals(Arrays.asList(expected), actual);		
+	}
+
 }
