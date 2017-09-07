a56d5ac21fa3be0c3b492d92e1113610f799ea71
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 14:23:50 +0800

    Fixed Failing test: multipleVariables

--- src/main/java/com/example/tutorials/tdd/testdriven/Template.java
+++ src/main/java/com/example/tutorials/tdd/testdriven/Template.java
@@ -1,18 +1,28 @@
 package com.example.tutorials.tdd.testdriven;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Template {
-	private String variableValue;
+	private Map<String, String> variables;
 	private String templateText;
 	
 	public Template(String templateText) {
+		this.variables = new HashMap<String, String>();
 		this.templateText = templateText;
 	}
 	
-	public void set(String variable, String value) {
-		this.variableValue = value;
+	public void set(String name, String value) {
+		this.variables.put(name, value);
 	}
 	
 	public String evaluate() {
-		return templateText.replaceAll("\\$\\{name\\}", variableValue);
+		String result = templateText;
+		for(Entry<String, String> entry : variables.entrySet()) {
+			String regex = "\\$\\{" + entry.getKey() + "\\}";
+			result = result.replaceAll(regex, entry.getValue());
+		}
+		return result;
 	}
 }
