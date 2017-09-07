ec0be714b4271e2af5c87bee41c1f9f3096b1bb5
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 13:12:32 +0800

    Initial Maven Project

---
+++ src/main/java/com/example/tutorials/tdd/testdriven/App.java
@@ -0,0 +1,13 @@
+package com.example.tutorials.tdd.testdriven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
