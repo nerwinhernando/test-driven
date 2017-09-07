3dbaff524523f3dfee8542ab0a37c410c7820d3d
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 16:23:00 +0800

    Refactor: removed duplicates in test

--- src/test/java/com/example/tutorials/tdd/testdriven/TestTemplateParse.java
+++ src/test/java/com/example/tutorials/tdd/testdriven/TestTemplateParse.java
@@ -9,17 +9,19 @@ import org.junit.Test;
 public class TestTemplateParse {
 	@Test
 	public void emptyTemplateRendersAsEmptyString() throws Exception {
-		TemplateParse parse = new TemplateParse();
-		List<String> segments = parse.parse("");
+		List<String> segments = parse("");
 		assertEquals("Number of segments", 1, segments.size());
 		assertEquals("", segments.get(0));
 	}
 	
 	@Test
 	public void templateWithOnlyPlainText() throws Exception {
-		TemplateParse parse = new TemplateParse();
-		List<String> segments = parse.parse("plain text only");
+		List<String> segments = parse("plain text only");
 		assertEquals("Number of segments", 1, segments.size());
 		assertEquals("plain text only", segments.get(0));
+	}
+
+	private List<String> parse(String template) {
+		return new TemplateParse().parse(template);
 	}
 }
