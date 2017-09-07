c65390dcdb23a31d6fa9368d8abebf79b43f20f2
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-07 22:33:39 +0800

    Refactored levels of abstraction

--- src/main/java/com/example/tutorials/tdd/testdriven/Template.java
+++ src/main/java/com/example/tutorials/tdd/testdriven/Template.java
@@ -20,6 +20,10 @@ public class Template {
 	public String evaluate() {
 		TemplateParse parser = new TemplateParse();
 		List<String> segments = parser.parse(templateText);
+		return concatenate(segments);
+	}
+
+	private String concatenate(List<String> segments) {
 		StringBuilder result = new StringBuilder();
 		for(String segment: segments) {
 			append(segment, result);
@@ -28,15 +32,23 @@ public class Template {
 	}
 
 	private void append(String segment, StringBuilder result) {
-		if(segment.startsWith("{") && segment.endsWith("}")) {
-			String var = segment.substring(2, segment.length() -1);
-			if(!variables.containsKey(var)) {
-				throw new MissingValueException("No value for " + segment);
-			}
-			result.append(variables.get(var));
+		if(isVariable(segment)) {
+			evaluateVariable(segment, result);
 		} else {
 			result.append(segment);
 		}
 		
+	}
+
+	private boolean isVariable(String segment) {
+		return segment.startsWith("{") && segment.endsWith("}");
+	}
+
+	private void evaluateVariable(String segment, StringBuilder result) {
+		String var = segment.substring(2, segment.length() -1);
+		if(!variables.containsKey(var)) {
+			throw new MissingValueException("No value for " + segment);
+		}
+		result.append(variables.get(var));
 	}
 }
