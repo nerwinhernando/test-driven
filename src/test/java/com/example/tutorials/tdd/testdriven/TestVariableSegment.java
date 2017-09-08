67b4123f2718f24a98e73f8894e6033077831211
Nerwin Hernando <nerwinc.hernando@gmail.com>
2017-09-08 17:46:54 +0800

    Refactoring Test Variable Segment

--- src/test/java/com/example/tutorials/tdd/testdriven/TestVariableSegment.java
+++ src/test/java/com/example/tutorials/tdd/testdriven/TestVariableSegment.java
@@ -5,15 +5,35 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestVariableSegment {
+	private Map<String, String> variables;
+	
+	@Before
+	public void setUp() {
+		variables = new HashMap<String, String>();
+	}
+	
 	@Test
 	public void variableEvaluatesToItsValue() throws Exception {
-		Map<String, String> variables = new HashMap<String, String>();
 		String name = "myvar";
 		String value = "myvalue";
 		variables.put(name, value);
 		assertEquals(value, new Variable(name).evaluate(variables));
+	}
+	
+	@Test
+	public void missingVariablesRaisesException() throws Exception {
+		String name = "myvar";
+		try {
+			new Variable(name).evaluate(variables);
+			fail("Missing variable value should raise an exception");
+			
+		} catch (MissingValueException expected){
+			assertEquals("No value for ${" + name + "}",
+					expected.getMessage());
+		}
 	}
 }
